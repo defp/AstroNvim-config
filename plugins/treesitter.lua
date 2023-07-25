ac68d66d29a5c43bb6c267b83d2f6a9cce0c22af
dåshuång li <li.dashuang@icloud.com>
2023-07-26 02:20:50 +0800

    format

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,8 +1,7 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-        -- add more things to the ensure_installed table protecting against community packs modifying it
-        opts.ensure_installed = require("astronvim.utils").list_insert_unique(
-                                    opts.ensure_installed, {"elixir", "heex"})
-    end
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, { "elixir", "heex" })
+  end,
 }
