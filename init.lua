3ee4f19a39b0fbae70b240f4b91f98a6509c5ca4
dåshuång li <li.dashuang@icloud.com>
2023-07-27 18:52:57 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -31,7 +31,8 @@ return {
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
+          "go",
+          "python",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
@@ -40,6 +41,8 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         "lua_ls",
+        "yamlfmt",
+        "prettier"
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
