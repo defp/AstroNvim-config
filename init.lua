17745075e742b6b836df13f33dcb362297548930
dåshuång li <li.dashuang@icloud.com>
2023-08-08 17:27:41 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -18,8 +18,8 @@ return {
   },
 
   -- Set colorscheme to use
-  -- colorscheme = "astrodark",
-  colorscheme = "sonokai",
+  colorscheme = "astrodark",
+  -- colorscheme = "sonokai",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = { virtual_text = true, underline = true },
@@ -33,6 +33,7 @@ return {
         allow_filetypes = { -- enable format on save for specified filetypes only
           "go",
           "python",
+          "elixir"
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
@@ -41,8 +42,6 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         "lua_ls",
-        "yamlfmt",
-        "prettier"
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
