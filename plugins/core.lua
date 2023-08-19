72643ba20c42f519162ff0741e6b5cc8166690c9
dåshuång li <li.dashuang@icloud.com>
2023-08-20 00:23:14 +0800

    golang config

--- plugins/core.lua
+++ plugins/core.lua
@@ -101,5 +101,19 @@ return {
     end,
     dependencies = { "nvim-lua/plenary.nvim" },
   },
-  { 'projekt0n/github-nvim-theme' }
+  { 'projekt0n/github-nvim-theme' },
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  }
 }
